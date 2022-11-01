e4379e2043a4190ecf8a1f43b1ed60d01fff376a
Emanoel Galdino Cardoso <eusouemanoel@gmail.com>
2022-11-01 19:29:53 -0300

    Lista em Python
    Introdução a lista em Python, pontos importantes e exemplos simples.

---
+++ Tema - Listas/list_in_python.py
@@ -0,0 +1,26 @@
+"""
+INTRODUÇÃO EM LISTA:
+
+Informações importantes sobre Lista:
+
+- Lista é considerada um objeto em Python;
+- Listas em Python, foram feitas para serem homogêneas, entretanto, aceitam valores héterogêneos;
+
+ESTRUTURA DE UMA LISTA:
+
+nome_lista = [valor, valor, valor, valor]
+
+"""
+
+# Prática - Criando listas simples:
+
+# Lista de amigos:
+nome_amigos = ["Marcelo", "Denilson", "André", "Gabriel", "Eliseu"]
+
+# Lista de números aleatórios:
+numeros = [0, 1, 3, 6, 4, 9, 6, 4, 5]
+
+"""
+Ambas as listas são homogêneas, abaixo uma lista héterogênea:
+"""
+lista_heterogenea = ["Pessoa", 15, "oi", 59, "Arroz", 80]
